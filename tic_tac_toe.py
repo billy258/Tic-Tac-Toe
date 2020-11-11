b396ded1971d579f4cbe72eb454a8e928a66555c
william irving <64707280+goodtime258@users.noreply.github.com>
2020-11-11 22:20:02 +0000

    Got the input into the grid to work

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -5,9 +5,12 @@
 def empty_grid (grid):
     for row in grid:
         for cell in row:
-            if cell == 0:
-                return False
-    return True
+            if cell != 0:
+                continue
+            else: return True
+    return False 
+
+    
 
 
 grid = [
@@ -17,5 +20,21 @@ grid = [
 ]
 
 while True:
-    for i in range(9):
-        if 
+
+    player1 = input('where on the grid would you like to go?\n')
+    
+    if empty_grid(grid) == False :break
+    
+    if player1 == '1' : grid[0][0] = 'x'
+    elif player1 == '2' : grid[0][1] = 'x'
+    elif player1 == '3' : grid[0][2] = 'x'
+    elif player1 == '4' : grid[1][0] = 'x'
+    elif player1 == '5' : grid[1][1] = 'x'
+    elif player1 == '6' : grid[1][2] = 'x'
+    elif player1 == '7' : grid[2][0] = 'x'
+    elif player1 == '8' : grid[2][1] = 'x'
+    elif player1 == '9' : grid[2][2] = 'x'
+
+    else: print('enter a valid grid value')
+
+    print(grid)
