aa65eba29d3528b725fe4e27ce4cc3adddde40f6
william irving <64707280+goodtime258@users.noreply.github.com>
2020-11-11 23:14:08 +0000

    Improved the grid square selector and a way to detect if a square has been taken

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,6 +2,11 @@
 # To win the player must get a 3 in a row vertically, horonzontally and 
 # diagonally. 
 
+grid = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
 def empty_grid (grid):
     for row in grid:
         for cell in row:
@@ -10,31 +15,39 @@ def empty_grid (grid):
             else: return True
     return False 
 
-    
-
-
-grid = [
-    [0,0,0],
-    [0,0,0],
-    [0,0,0]
-]
 
 while True:
-
-    player1 = input('where on the grid would you like to go?\n')
     
     if empty_grid(grid) == False :break
+    player1 = input('where on the grid would you like to go?\n')
+    if player1 == 'quit':break
+    valid = False
     
-    if player1 == '1' : grid[0][0] = 'x'
-    elif player1 == '2' : grid[0][1] = 'x'
-    elif player1 == '3' : grid[0][2] = 'x'
-    elif player1 == '4' : grid[1][0] = 'x'
-    elif player1 == '5' : grid[1][1] = 'x'
-    elif player1 == '6' : grid[1][2] = 'x'
-    elif player1 == '7' : grid[2][0] = 'x'
-    elif player1 == '8' : grid[2][1] = 'x'
-    elif player1 == '9' : grid[2][2] = 'x'
+    try:
+        iplayer1 = int(player1)
+    except:
+        print('Please enter a number between 1 and 9')
 
-    else: print('enter a valid grid value')
+    for i in range(9):
+        row = i // 3
+        col = i % 3
+        selection = iplayer1 - 1
+        if i == selection and grid[row][col] == 0:
+            grid[row][col] = 'X'
+            break
+        if i == selection and grid[row][col] != 0:
+            valid = True
+            break
+        
 
-    print(grid)
+    for r in grid:
+        print(*r)
+        print('-----')
+    if valid == True:
+        print('Not a valid move.')
+
+    
+        
+
+    
+    
