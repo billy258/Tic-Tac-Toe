7cf88ef0b4c7eed20eca6dd02286582b884fa3a3
william irving <64707280+goodtime258@users.noreply.github.com>
2020-11-12 11:47:49 +0000

    Added a row win to the grid and improved readability when printed in terminal.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -15,18 +15,30 @@ def empty_grid (grid):
             else: return True
     return False 
 
-
-while True:
+def row_checker (grid,row,col):
+    first = grid[row][col]
+    for j in grid[row]:
+        if j != first or j == 0:
+            return False
+    return True
     
-    if empty_grid(grid) == False :break
+
+
+
+while empty_grid(grid):
+    
     player1 = input('where on the grid would you like to go?\n')
     if player1 == 'quit':break
     valid = False
+    win = False
     
     try:
         iplayer1 = int(player1)
+        if iplayer1 < 0 or iplayer1 > 9: 
+            raise Exception
+            
     except:
-        print('Please enter a number between 1 and 9')
+        valid = True
 
     for i in range(9):
         row = i // 3
@@ -34,17 +46,29 @@ while True:
         selection = iplayer1 - 1
         if i == selection and grid[row][col] == 0:
             grid[row][col] = 'X'
+            if row_checker(grid,row,col) == True:
+                win = True
+                win_player = grid[row][0]
+                break
             break
         if i == selection and grid[row][col] != 0:
             valid = True
             break
         
-
+            
+        
+    print('\n')
     for r in grid:
         print(*r)
         print('-----')
+    print('\n')
     if valid == True:
         print('Not a valid move.')
+    if win == True:
+        print(win_player+' has won')
+        break
+        
+
 
     
         
